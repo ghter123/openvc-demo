b2278cf95d7b18c36f25afcb0b2635ab05dc9141
qin <coronel.liu@coolkit.cn>
2022-08-29 15:30:51 +0800

    init

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿using Emgu.CV;
+using Emgu.CV.Structure;
+
+var image = new Image<Bgr, Byte>(200, 200);
+image.Save("aa.png");
+var a = CvInvoke.Imread("aa.png");
+
+Console.WriteLine("hello world");
